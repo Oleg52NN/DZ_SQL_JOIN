622d8695374a39f94e805b352352fffd797efc60
Oleg NN <zvoprosno@gmail.com>
2023-05-17 14:08:48 +0300

    fixed

--- jpin.sql
+++ jpin.sql
@@ -1,4 +1,4 @@
 SELECT product_name FROM Person.ORDERS
     RIGHT JOIN Person.CUSTOMERS
-    ON lower(name) = 'alexey'
-    WHERE CUSTOMERS.id  = customer_id;
+    ON CUSTOMERS.id  = customer_id
+    WHERE lower(name) = 'alexey';
